1e86cde54c3d96f6c3ab5a1c64972e55e23c5270
Hannie Dong <hdong92@gmail.com>
2018-01-26 13:59:13 -0800

    Create User and Posting model.

---
+++ db/migrate/20180126215157_create_postings.rb
@@ -0,0 +1,11 @@
+
+class CreatePostings < ActiveRecord::Migration[5.1]
+  def change
+    create_table :postings do |t|
+      t.string :latitude, null: false
+      t.string :longitude, null: false
+      t.integer :likes
+      t.timestamps
+    end
+  end
+end
