11df4a30beaf54815b5c5f250997d1c4ece644b3
Hannie Dong <hdong92@gmail.com>
2018-01-26 13:38:52 -0800

    Create HomePage component. Delete hello world component.

---
+++ client/app/bundles/HomePage/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1> ViewPoints </h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
--- client/app/bundles/HomePage/startup/registration.jsx
+++ client/app/bundles/HomePage/startup/registration.jsx
@@ -1,8 +1,7 @@
 import ReactOnRails from 'react-on-rails';
-
-import HelloWorld from '../components/HelloWorld';
+import HomePage from '../components/HomePage';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
+  HomePage,
 });
