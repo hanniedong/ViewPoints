39f57da53909a033741c1cb8c86cc7f794cd78b9
Hannie Dong <hdong92@gmail.com>
2018-01-30 15:19:40 -0800

    zoom, drag, and drop the map and save its coordinates.

--- client/app/bundles/HomePage/components/Map.jsx
+++ client/app/bundles/HomePage/components/Map.jsx
@@ -4,9 +4,21 @@ import { PlaceMarker } from './PlaceMarker'
 
 const ViewMap = withGoogleMap(props => (
   <GoogleMap
+    ref = {props.onMapMounted}
+    onZoomChanged = {props.handleMapChanged}
+    onDragEnd= {props.handleMapChanged}
+    onBoundsChanged = {props.handleMapFullyLoaded}
     defaultCenter={props.center}
-    defaultZoom={props.zoom} >
-    {props.places}
+    defaultZoom={props.zoom} 
+  >
+        {console.log(props)}
+    {props.places.length > 0 && props.places.map(place =>(
+      <PlaceMarker 
+        lat={37.781555}
+        lng= {-122.393990}
+        name= {'Park'}
+      />
+    ))}
   </GoogleMap>
 ));
  
@@ -15,25 +27,81 @@ export default class Map extends Component {
     super(props)
  
     this.zoom = 13
- 
+    this.xMapBounds = {min: null, max: null}
+    this.yMapBounds = {min: null, max: null}
+    this.mapFullyLoaded = false
+
     this.state = {
+      places: [],
       lat: 37.781555,
       lng: -122.393990
     };
   }
+
+  handleMapChanged() {
+    this.getMapBounds()
+    this.setMapCenterPoint()
+    this.fetchPlacesFromApi()
+  }
+ 
+   handleMapMounted(map) {
+    this.map = map
+  }
+
+  handleMapFullyLoaded() {
+    if (this.mapFullyLoaded)
+      return
+ 
+    this.mapFullyLoaded = true
+    this.handleMapChanged()
+  }
+
+  setMapCenterPoint() {
+    this.setState({
+      lat: this.map.getCenter().lat(),
+      lng: this.map.getCenter().lng()
+    })
+  }
+
+  fetchPlacesFromApi() {
+    const place = <PlaceMarker lat={37.781555} lng={-122.393990} name={"Park"} />
+    this.setState({ places: [place] })
+  }
+ 
+  getMapBounds() {
+    var mapBounds = this.map.getBounds()
+    var xMapBounds = mapBounds.b
+    var yMapBounds = mapBounds.f
+ 
+    this.xMapBounds.min = xMapBounds.b
+    this.xMapBounds.max = xMapBounds.f
+ 
+    this.yMapBounds.min = yMapBounds.f
+    this.yMapBounds.max = yMapBounds.b
+  }
  
   render() {
-    const {lat, lng} = this.state;
-    const places = [<PlaceMarker lat={lat} lng={lng} price={20} name={"Hotel"} description={"Hotel desc"} />]
+    const {lat, lng, places} = this.state;
     return(
       <div style={{width: `750px`, height: `750px`}}>
+        <ul>
+          <li>lng: {lng}</li>
+          <li>lat: {lat}</li>
+          <li>xMapBounds.min: {this.xMapBounds.min}</li>
+          <li>xMapBounds.max: {this.xMapBounds.max}</li>
+          <li>yMapBounds.min: {this.yMapBounds.min}</li>
+          <li>yMapBounds.max: {this.yMapBounds.max}</li>
+        </ul>
         <ViewMap
+          onMapMounted={this.handleMapMounted.bind(this)}
+          handleMapChanged={this.handleMapChanged.bind(this)}
+          handleMapFullyLoaded={this.handleMapFullyLoaded.bind(this)}
           center={{
             lat: lat,
             lng: lng
           }}
+          places={places}
           zoom={this.zoom}
-          places = {places}
           containerElement={
             <div style={{ height: `100%` }} />
           }
